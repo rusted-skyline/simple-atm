6d85516e4a4a4f2cd05e9145086bc934db33958a
Rusted Skyline <esroberts+github.alt@gmail.com>
2021-04-30 17:23:18 -0500

    clean up scripts for better presentation

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-./gradlew build && java -jar build/libs/atm-0.0.1-SNAPSHOT.jar
+JAR_FILE="build/libs/atm-0.0.1-SNAPSHOT.jar"
+
+if [ -f "$JAR_FILE" ]; then
+	java -jar $JAR_FILE
+else
+	echo "Please run ./build.sh first to build the jar artifact"
+fi
